e3556904a78a5054556a218e65cf62967a93cffe
Sergey Zelenov <zelenov@so1.ai>
2021-12-17 18:02:08 +0100

    init

---
+++ app.js
@@ -0,0 +1,33 @@
+const path = require('path');
+const Koa = require('koa');
+const app = new Koa();
+
+app.use(require('koa-static')(path.join(__dirname, 'public')));
+app.use(require('koa-bodyparser')());
+
+const Router = require('koa-router');
+const router = new Router();
+
+const clients = new Set();
+router.get('/subscribe', async (ctx, next) => {
+  const message = await new Promise((resolve, reject) => {
+    clients.add(resolve);
+    ctx.req.on('aborted', resolve);
+  });
+  ctx.body = message;
+});
+
+router.post('/publish', async (ctx, next) => {
+  const {message} = ctx.request.body;
+  if (!message) ctx.throw(400, 'no message');
+
+  clients.forEach((resolve) => {
+    resolve(message);
+  });
+  clients.clear();
+  ctx.body = 'ok!';
+});
+
+app.use(router.routes());
+
+module.exports = app;
